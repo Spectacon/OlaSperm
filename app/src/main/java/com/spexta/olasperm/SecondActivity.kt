66d3d81099e340b2dcf079308dbb50b719197820
Ghost Sams <personalmail3305@gmail.com>
2020-12-31 03:39:35 +0100

    Third Commit Banner Ads are now working moving to interstitial Ads now

--- app/src/main/java/com/spexta/olasperm/SecondActivity.kt
+++ app/src/main/java/com/spexta/olasperm/SecondActivity.kt
@@ -1,11 +1,33 @@
 package com.spexta.olasperm
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.MobileAds
 
 class SecondActivity : AppCompatActivity() {
+
+    lateinit var mAdView : AdView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+        MobileAds.initialize(this) {}
+
+        val BannerBtn = findViewById<Button>(R.id.banner)
+
+        BannerBtn.setOnClickListener() {
+            val intent = Intent(this, BannerActivity::class.java)
+            startActivity(intent)
+
+//        Banner admob  codes
+            mAdView = findViewById(R.id.adView)
+            val adRequest = AdRequest.Builder().build()
+            mAdView.loadAd(adRequest)
+
+        }
     }
 }
